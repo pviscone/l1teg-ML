5f96baaafb144fb0e71127c2009c40198c6f1abd
Piero Viscone <pieroviscone@gmail.com>
2025-07-24 21:31:18 +0200

    fixed eras in step0

--- tasks/EB_ptRegr/step0_ntuple/flows/base_flow.py
+++ tasks/EB_ptRegr/step0_ntuple/flows/base_flow.py
@@ -9,7 +9,7 @@ def flow(dR_genMatch = 0.1):
         DefineSkimmedCollection("GenEl", mask = "abs(GenEl_caloeta)<1.479"),
         Cut("EB", "nGenEl==2"),
         DefineSkimmedCollection("TkEleL2", mask="abs(TkEleL2_caloEta)<1.479", eras = ["140Xv0C1"]),             #on PU0 sample
-        DefineSkimmedCollection("TkEleL2", mask="TkEleL2_in_caloPt>0", eras = ["151Xv0pre4_TkElePtRegr_dev"]),  #on PU200 sample (custom)
+        DefineSkimmedCollection("TkEleL2", mask="TkEleL2_in_caloPt>0", eras = ["151Xv0pre4_TkElePtRegr_dev_withScaled"]),  #on PU200 sample (custom)
         Define("TkEleL2_genIdx", f"match(TkEleL2_caloEta, TkEleL2_caloPhi, GenEl_caloeta, GenEl_calophi, {dR_genMatch})"), #Cut also on dz?
         Define("GenEl_nMatch", "count_matched(TkEleL2_genIdx, nGenEl)"),
         DefineSkimmedCollection("TkEle", "TkEleL2", mask = "TkEleL2_genIdx!=-1"),
