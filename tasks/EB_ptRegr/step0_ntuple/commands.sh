e186b957b86fe739fc132006f48207659cb7f7b0
Piero Viscone <pieroviscone@gmail.com>
2025-08-05 19:39:50 +0200

    updated step0 commands

--- tasks/EB_ptRegr/step0_ntuple/commands.sh
+++ tasks/EB_ptRegr/step0_ntuple/commands.sh
@@ -1,2 +1,5 @@
-run_analysis.py --cfg cfg/l1teg_cfg.py  --mc data/DoubleElePU0.py  --flow flows/base_flow.py   --plots plots/base_plots.py  -o tempPU0 --eras "140Xv0C1" --cache
-run_analysis.py --cfg cfg/l1teg_cfg.py  --mc data/DoubleElePU200.py  --flow flows/base_flow.py   --plots plots/base_plots.py  -o tempPU200 --eras "151Xv0pre4_TkElePtRegr_dev_withScaled" --snapshot --columnSel "TkEle_(.*),nTkEle" --cache
+#Big training dataset
+run_analysis.py --cfg cfg/l1teg_cfg.py  --mc data/DoubleElePU200.py --flow flows/base_flow.py   --plots plots/base_plots.py  -o tempTrain --eras "151Xv0pre4_TkElePtRegr_dev_withScaled" --snapshot --columnSel "TkEle_(.*),nTkEle" --cache
+
+#Test Dataset with corrected pt
+run_analysis.py --cfg cfg/l1teg_cfg.py  --mc data/DoubleElePU200.py --flow flows/base_flow.py   --plots plots/base_plots.py  -o tempA2 --eras "151X_ptRegr_v0_A2" --snapshot --columnSel "TkEle_(.*),nTkEle" --cache
