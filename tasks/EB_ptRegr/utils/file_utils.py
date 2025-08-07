0987a84e25d1d1d99c56ef968e84b424e9e88acd
Piero Viscone <pieroviscone@gmail.com>
2025-08-07 21:24:37 +0200

    minor fix

--- tasks/EB_ptRegr/utils/file_utils.py
+++ tasks/EB_ptRegr/utils/file_utils.py
@@ -6,7 +6,8 @@ import pandas as pd
 def openAsDataframe(path, collection):
     file = uproot.open(path)["Events"]
     keys = file.keys()
-    keys.remove(f"n{collection}")
+    if f"n{collection}" in keys:
+        keys.remove(f"n{collection}")
     arrays = file.arrays(keys)
     return pd.DataFrame({key: ak.flatten(arrays[key]) for key in keys})
 # %%
